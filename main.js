28c55efeee7d12008f3b8e16eee8e31db5407a98
jujuba31072008 <ana.barz.santos@escola.pr.gov.br>
2026-05-28 14:52:13 +0000

    AULA 8 - COMMIT

--- main.js
+++ main.js
@@ -59,3 +59,31 @@ function formatarMilissegundos(ms) {
 
   return resultado;
 }
+
+comecaCronometro();
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+  document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+  document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
+}
